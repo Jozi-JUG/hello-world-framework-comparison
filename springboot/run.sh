a385f7d79979ce5ae6b068b8938754623071914e
corneil_jumpco <corneil@jumpco.io>
2021-06-30 13:22:51 +0200

    updated README.md

--- springboot/run.sh
+++ springboot/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source ~/.sdkman/bin/sdkman-init.sh
 sdk use java 16.0.1-zulu
-java -Xmx64M -Xms64M -jar ./target/springboot-0.0.1-SNAPSHOT.jar
+java -Xmx50M -Xms50M -jar ./target/springboot-0.0.1-SNAPSHOT.jar
